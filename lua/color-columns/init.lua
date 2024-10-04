07f8cfabec45bb142eadecae5bd460bf290652bc
Aaron DeWitt <Aaron.DeWitt@vu.com>
2024-10-04 17:11:08 -0500

    feat: added configuration

--- lua/color-columns/init.lua
+++ lua/color-columns/init.lua
@@ -3,13 +3,16 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
+	local local_position = opts.position or "80,100,120"
+	local key = opts.key_binding or "<leader>uG"
+
 	vim.g.color_column_enabled = true
-	vim.opt.colorcolumn = "80,100,120"
+	vim.opt.colorcolumn = local_position
 
 	vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
 		callback = function()
 			if vim.g.color_column_enabled then
-				vim.opt.colorcolumn = "80,100,120"
+				vim.opt.colorcolumn = local_position
 			else
 				vim.opt.colorcolumn = "0"
 			end
@@ -19,11 +22,11 @@ function M.setup(opts)
 	local wk = require("which-key")
 	wk.add({
 		{
-			"<leader>uG",
+			key,
 			function()
 				vim.g.color_column_enabled = not vim.g.color_column_enabled
 				if vim.g.color_column_enabled then
-					vim.opt.colorcolumn = "80,100,120"
+					vim.opt.colorcolumn = local_position
 				else
 					vim.opt.colorcolumn = "0"
 				end
